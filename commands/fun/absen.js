baecb73923672125e75aa360cf8ecfe50dac09f8
moez-rd <moez.muhardin@gmail.com>
2022-04-07 12:28:37 +0700

    Update morning

--- commands/fun/absen.js
+++ commands/fun/absen.js
@@ -1,9 +1,10 @@
+import moment from 'moment';
+
 module.exports = {
     name: 'absen',
     description: 'hanya absen',
     cooldown: 60,
     execute(message, args) {
-        const datetime = Date.now();
-        message.channel.send(`${message.author}, kamu berhasil absen pada jam ${datetime.getHours()} lewat ${datetime.getMinutes()} menit.`);
+        message.channel.send(`${message.author}, kamu berhasil absen pada ${moment().format('LLLL')}.`);
     },
 };
