9cff2bf90f84b0bd739f92a8fcff9bcda739fe40
moez-rd <moez.muhardin@gmail.com>
2022-04-07 12:38:47 +0700

    Fix bug

--- commands/fun/absen.js
+++ commands/fun/absen.js
@@ -5,6 +5,6 @@ module.exports = {
     description: 'hanya absen',
     cooldown: 60,
     execute(message, args) {
-        message.channel.send(`${message.author}, kamu berhasil absen pada ${moment().format('LLLL')}.`);
+        message.channel.send(`${message.author}, kamu berhasil absen pada ${moment().tz('Asia/Jakarta').format('LLLL')}.`);
     },
 };
