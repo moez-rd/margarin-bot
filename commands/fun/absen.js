49522daadb630b2a9dce3066af181f823454c714
moez-rd <moez.muhardin@gmail.com>
2022-04-07 12:08:19 +0700

    Add absen

--- commands/fun/absen.js
+++ commands/fun/absen.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'hanya absen',
     execute(message, args) {
         const datetime = new Date();
-        message.channel.send(`${message.author}, kamu berhasil absen pada ${datetime.getMinutes}:${datetime.getHours}`);
+        message.channel.send(`${message.author}, kamu berhasil absen pada ${datetime.getMinutes()}:${datetime.getHours()}`);
     },
 };
