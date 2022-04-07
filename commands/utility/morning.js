ba406d3c9c7b0da53e49fa5141982a4817ff3694
moez-rd <moez.muhardin@gmail.com>
2022-04-07 09:46:22 +0700

    gabut

--- commands/utility/morning.js
+++ commands/utility/morning.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'morning',
     description: 'Morning reminder',
     execute(message, args) {
-        let scheduledMessage = new cron.CronJob('* * * * * *', () => {
+        let scheduledMessage = new cron.CronJob('00 00 * * * *', () => {
             const embed = new Discord.MessageEmbed()
                 .setColor('#8FBDD3')
                 .setTitle('Selamat pagy masyarakat NgodingVareng')
